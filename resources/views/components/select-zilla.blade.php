bed386f03c4f8b972b37e5f24b7893db548ff049
seaum23 <seaum333@gmail.com>
2021-12-28 19:30:48 +0600

    SELECT ZILLA

---
+++ resources/views/components/select-zilla.blade.php
@@ -0,0 +1,64 @@
+<div>
+<option>Chittagong-চট্টগ্রাম</option>
+<option>Dhaka – ঢাকা</option>
+<option>Rajshahi – রাজশাহী</option>
+<option>Sylhet – সিলেট</option>
+<option>Jessore – যশোর</option>
+<option>Dinajpur - দিনাজপুর</option>
+<option>Mymensingh - ময়মনসিংহ</option>
+<option>Comilla – কুমিল্লা</option>
+<option>Barisal – বরিশাল</option>
+<option>Bogra – বগুড়া</option>
+<option>Pabna – পাবনা
+<option>Rangamati - রাঙ্গামাটি</option>
+<option>Kushtia – কুষ্টিয়া</option>
+<option>Rangpur – রংপুর</option>
+<option>Noakhali - নোয়াখালী</option>
+<option>Khulna – খুলনা</option>
+<option>Tangail – টাঙ্গাইল</option>
+<option>Panchagarh - পঞ্চগড়</option>
+<option>Bhola - ভোলা</option>
+<option>Habiganj – হবিগঞ্জ</option>
+<option>Lakshmipur - লক্ষীপুর</option>
+<option>Barguna – বরগুনা</option>
+<option>Jhalokati – ঝালকাঠি</option>
+<option>Pirojpur - পিরোজপুর</option>
+<option>Patuakhali - পটুয়াখালী</option>
+<option>Jhenaidah - ঝিনাইদহ</option>
+<option>Narail – নড়াইল</option>
+<option>Magura – মাগুরা</option>
+<option>Lalmonirhat - লালমনিরহাট</option>
+<option>Kurigram - কুড়িগ্রাম</option>
+<option>Nilphamari - নীলফামারী</option>
+<option>Gaibandha – গাইবান্ধা</option>
+<option>Thakurgaon - ঠাকুরগাঁ</option>
+<option>Satkhira – সাতক্ষিরা</option>
+<option>Bagerhat - বাগেরহাট</option>
+<option>Chuadanga - চুয়াডাঙ্গা</option>
+<option>Meherpur - মেহেরপুর</option>
+<option>Sirajganj - সিরাজগঞ্জ</option>
+<option>Joypurhat - জয়পুরহাট</option>
+<option>Natore – নাটোর</option>
+<option>Naogaon – নওগাঁ</option>
+<option>Nawabganj - নওয়াবগঞ্জ</option>
+<option>Khagrachhari - খাগড়াছড়ি</option>
+<option>Feni – ফেনী</option>
+<option>Brahmanbaria - ব্রাহ্মণবাড়ীয়া</option>
+<option>Sunamganj - সুনামগঞ্জ</option>
+<option>Cox's Bazar - কক্সবাজার</option>
+<option>Moulvibazar -মৌলভীবাজার</option>
+<option>Gopalganj - গোপালগঞ্জ</option>
+<option>Shariatpur - শরীয়তপুর</option>
+<option>Madaripur - মাদারীপুর</option>
+<option>Rajbari – রাজবাড়ী</option>
+<option>Gazipur – গাজীপুর</option>
+<option>Kishoreganj - কিশোরগঞ্জ</option>
+<option>Jamalpur - জামালপুর</option>
+<option>Sherpur – শেরপুর</option>
+<option>Netrakona - নেত্রকোনা</option>
+<option>Munshiganj - মুন্সীগঞ্জ</option>
+<option>Narsingdi – নরসিংদী</option>
+<option>Narayanganj -নারায়ণগঞ্জ</option>
+<option>Manikganj - মানিকগঞ্জ</option>
+
+</div>
